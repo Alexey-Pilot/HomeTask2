22a5a45f5307cd31efdd2d2d539e1a2802d456f1
Alexey <alexragnarek@gmail.com>
2023-01-17 14:14:13 +0500

    ex13 is complited

---
+++ ex13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Insert the number ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99)
+{
+  while (num > 999)
+  {
+    num = num / 10;
+  }
+  num = num % 10;
+  Console.WriteLine($"The third digit {num}");
+}
+else
+{
+  Console.WriteLine("There is no 3rd digit");
+}
