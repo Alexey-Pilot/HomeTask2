160607c647d99ca307c88392f9dc640451802f5a
Alexey <alexragnarek@gmail.com>
2023-01-17 14:28:39 +0500

    ex15 is complited

---
+++ ex15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер дня");
+int day = Convert.ToInt32(Console.ReadLine());
+while (day > 7 || day < 1)
+{
+  Console.WriteLine("В неделе 7 дней");
+  day = Convert.ToInt32(Console.ReadLine());
+}
+
+
+if (day < 6)
+{
+  Console.WriteLine("Нет!");
+}
+else{
+  Console.WriteLine("Да!");
+}
