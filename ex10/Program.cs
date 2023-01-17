603fac81947b4967978e56f23f5ff0c9442414be
Alexey <alexragnarek@gmail.com>
2023-01-17 13:46:50 +0500

    ex10 is complited

---
+++ ex10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+while (num < 100 || num > 999)
+{
+  Console.WriteLine("Введите трехзначное число!!!");
+  num = Convert.ToInt32(Console.ReadLine());
+}
+int num2 = num % 100 / 10;
+Console.WriteLine($"Вторая цифра чила {num} это {num2}");
