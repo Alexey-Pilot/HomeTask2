b6a1600d464d777549c3456f7a0df43cba8a9aaf
Alexey <alexragnarek@gmail.com>
2023-01-17 14:47:16 +0500

    Extra is complited

---
+++ extra/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Вводите числа, чтобы прекратить нажмите 0");
+int i = Convert.ToInt32(Console.ReadLine());
+int max1 = i;
+int max2 = i;
+while (i != 0)
+{
+  i = Convert.ToInt32(Console.ReadLine());
+  if (i > max1)
+  {
+    max2 = max1;
+    max1 = i;  
+  }
+  else if (i > max2 && i < max1)  // на случай, если встречаются одинаковые числа
+  max2 = i;
+
+}
+Console.WriteLine($"Второе по величине число {max2}");
